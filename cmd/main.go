65d1b03dd152e4bc3bc41764d23220b0bfbfbe28
Leonardo Nicola <leonardonicolares@gmail.com>
2024-04-17 16:07:07 -0300

    docs(swagger): intialize docs

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,26 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	_ "github.com/leonardonicola/tickethub/docs"
 	"github.com/leonardonicola/tickethub/internal/router"
 )
 
+//	@title			Tickethub
+//	@version		1.0
+//	@description	Serviço de ingressaria e tickets.
+
+//	@license.name	Apache 2.0
+//	@license.url	http://www.apache.org/licenses/LICENSE-2.0.html
+
+//	@host		localhost:3000
+//	@BasePath	/api/v1
+
+//	@securityDefinitions.apikey JWT
+//  @in header
+//  @name Authorization
+
+// @externalDocs.description	OpenAPI
+// @externalDocs.url			https://swagger.io/resources/open-api/
 func main() {
 	r := gin.Default()
 	router.InitRoutes(r)
