cf9efa8816fd977a9dc2bc0d2c9c7ed936f888d4
Leonardo Nicola <leonardonicolares@gmail.com>
2024-04-17 16:07:55 -0300

    feat: add output and login dtos

--- internal/dto/user.go
+++ internal/dto/user.go
@@ -1,9 +1,23 @@
 package dto
 
-type CreateUserDTO struct {
+type CreateUserInputDTO struct {
 	Name    string `json:"name" validate:"required,gte=1,lte=200"`
 	Surname string `json:"surname" validate:"required,gte=1,lte=200"`
 	Email   string `json:"email" validate:"required,email"`
 	Address string `json:"address" validate:"required,gte=1,lte=254"`
 	CPF     string `json:"cpf" validate:"required,gte=11,lte=11"`
+}
+
+type CreateUserOutputDTO struct {
+	ID      string
+	Name    string
+	Surname string
+	Email   string
+	Address string
+	CPF     string
+}
+
+type Login struct {
+	Email    string `validate:"required,email"`
+	Password string `validate:"required"`
 }
